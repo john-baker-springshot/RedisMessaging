7faad8af19f03e8973a932075e6af39352308d03
Kunjan Modi <kunjan.modi@springshot.com>
2016-05-27 14:21:44 -0700

    code refactoring

--- RedisMessaging/Producer/RedisProducer.cs
+++ RedisMessaging/Producer/RedisProducer.cs
@@ -7,35 +7,29 @@ namespace RedisMessaging.Producer
 {
   public class RedisProducer : IProducer
   {
-    public IConnection Connection { get; private set; }
+    public IConnection Connection { get; }
 
-    public IQueue MessageQueue { get; private set; }
+    public IQueue MessageQueue { get; }
 
     private readonly IConnectionMultiplexer _redis;
 
-    public RedisProducer(IConnection connection)
-    {
-      Connection = connection;
-      connection.Connect();
-      var redisConnection = (RedisConnection)Connection;
-      _redis = redisConnection.Multiplexer;
-    }
+    public RedisProducer(IConnection connection) : this(connection, null) { }
     
     public RedisProducer(IConnection connection, IQueue queue)
     {
       Connection = connection;
       MessageQueue = queue;
-      connection.Connect();
-      var redisConnection = (RedisConnection)Connection;
+
+      Connect();
+
+      var redisConnection = (RedisConnection) Connection;
       _redis = redisConnection.Multiplexer;
     }
 
-
-
     public virtual void Publish(string message)
     {
-      if (!Connection.IsConnected)
-        Connection.Connect();
+      Connect();
+
       if (string.IsNullOrEmpty(MessageQueue?.Name))
         throw new Exception("MessageQueue not initialized");
 
@@ -44,20 +38,25 @@ namespace RedisMessaging.Producer
 
     public virtual void Publish(string queue, string message)
     {
-      if (!Connection.IsConnected)
-        Connection.Connect();
+      Connect();
 
       _redis.GetDatabase().ListLeftPush(queue, message);
     }
 
     public virtual void Publish(IQueue queue, string message)
     {
-      if (!Connection.IsConnected)
-        Connection.Connect();
+      Connect();
+
       if (string.IsNullOrEmpty(queue?.Name))
         throw new Exception("Queue parameter not initialized");
 
       _redis.GetDatabase().ListLeftPush(queue.Name, message);
+    }
+
+    private void Connect()
+    {
+      if (!Connection.IsConnected)
+        Connection.Connect();
     }
   }
 }
